908ba159c9ee90387e7b8006443e8acc321c1d39
Zachery Reynolds <zachery.reynolds@stu.jefferson.kyschools.us>
2018-09-21 08:33:15 -0400

    Bo3 setup

--- Dictionaries_Project.py
+++ Dictionaries_Project.py
@@ -8,20 +8,43 @@ import sys
 import random
 
 
+while True:
+difficulties = ['Regular', 'Recruit', 'Hardened', 'Veteran']
 
-select_mission = [
-    'Black Ops',
-    'New World',
-    'In Darkness',
-    'Provocation',
-    'Hypocenter',
-    'Vengence',
-    'Rise & Fall',
-    'Demon Within',
-    'Sand Castle',
-    'Lotus Towers',
-    'Life'
-    ]
+user_profile = {
+    'username' : "default",
+    'rank' : "default",
+    }
+
+
+black_ops = 'Black Ops'
+new_world = 'New World'
+in_darkness = 'In Darkness'
+provocation = 'Provocation'
+hypocenter = 'Hypocnter'
+vengence = 'Vengence'
+rise_and_fall = 'Rise & Fall'
+demon_within = 'Demon Within'
+sand_castle = 'Sand Castle'
+lotus_tower = 'Lotus Tower'
+life = 'Life'
+
+
+select_mission = {
+    'black_ops':black_ops,
+    'new_world':new_world,
+    'in_darkness':in_darkness,
+    'provocation':provocation,
+    'hypocenter':hypocenter,
+    'vengence':vengence,
+    'rise_and_fall':rise_and_fall,
+    'demon_within':demon_within,
+    'sand_castle':sand_castle,
+    'lotus_towers':lotus_tower,
+    'life' : life,
+    }
+   
+
 
 
 
@@ -33,6 +56,10 @@ if start == 'f':
     time.sleep(1)
     print("*Ali A theme song*")
     time.sleep(3)
+
+user = input("Please enter your username")
+time.sleep(2)
+ran = input("What is your Rank?")
 
 
       
@@ -50,11 +77,65 @@ if camo == 'start_game':
     time.sleep(2)
     print("Begin")
 
-# Select_Mission
+# Join Game
+if camo == 'join_public_game':
+    print("Loading into game")
+    time.sleep(1)
+    print("Please wait")
+    time.sleep(3)
+    print("You may begin")
+
+#Change Difficulty
+if camo == 'change_difficulty':
+    print (difficulties)
+    dif = input("What difficulty setting would you like to play on?")
+    if dif == 'Regular':
+        print("You may begin*whispers* noob. ")
+    elif dif == 'Recruit':
+        print("YOU'RE JUST LIKE YOUR FATHER... Bigin. ")
+    elif dif == 'Hardened':
+        print("That's a little more like it. ")
+    elif dif == 'Veteran':
+        print("Now you're really a man. You're gonna die. ")
+    
+    
+    
+    
+
+
+
+
+    
+    
+# Select_Mission                                                    # INSERT PRINTS!!!!!!!!!!!!!!!!!!!!!!!!!
 if camo == 'select_mission':
-    print(select_mission)
+    for val in select_mission.values():
+        print(val)
     map_select = input("Which mission would you like to play?")
     if map_select == 'Black Ops':
-        print("This is where it all begins, try not to get yourself killed.")
+        print("This is where it all begins, try not to get yourself killed. ")
+    elif map_select == 'New World':
+        print("")
+    elif map_select == 'In Darkness':
+        print("")
+    elif map_select == 'Provocation':
+        print("")
+    elif map_select == 'Hypocenter':
+        print("")
+    elif map_select == 'Vengence':
+        print("")
+    elif map_select == 'Rise & Fall':
+        print("")
+    elif map_select == 'Demon Within':
+        print("")
+    elif map_select == 'Sand Castle':
+        print("")
+    elif map_select == 'Lotus Tower':
+        print("")
+    elif map_select == 'Life':
+        print("")
+
+
+    
  
     
